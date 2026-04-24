7d0abaa531169d50473de2e4cfb9c99635a1c261
NykolasDev <166623370+NykolasDev@users.noreply.github.com>
2026-04-23 21:04:05 -0300

    Update monitoramento.js

--- JS/monitoramento.js
+++ JS/monitoramento.js
@@ -1,47 +1,39 @@
 let dados = [];
 
 function buscarDados() {
-    fetch("http://localhost:1880/leituras")
-        .then(res => res.json())
+    fetch("http://10.110.12.13:1880/ler")
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("Erro HTTP: " + res.status);
+            }
+            return res.json();
+        })
         .then(data => {
 
-            let valores = data.reverse();
+            let valores = data.dados.reverse();
 
-            let ultimo = valores[valores.length - 1];
+            let ultimo = valores[0];
 
-          
-            document.getElementById("temperatura").innerText = ultimo.temperatura + " °C";
+            document.getElementById("temperatura").innerText =
+                ultimo.temperatura + " °C";
 
-            
-            document.getElementById("umidade").innerText = ultimo.umidade + " %";
+            document.getElementById("umidade").innerText =
+                ultimo.umidade + " %";
 
-           
             if (ultimo.temperatura > 30) {
                 document.getElementById("temperatura").style.color = "red";
             } else {
                 document.getElementById("temperatura").style.color = "#00c3ff";
             }
 
-            
-            let tabela = document.getElementById("tabela");
-            tabela.innerHTML = "";
-
-            valores.forEach(d => {
-                tabela.innerHTML += `
-                    <tr>
-                        <td>${new Date(d.data).toLocaleString()}</td>
-                        <td>${d.temperatura} °C</td>
-                        <td>${d.umidade} %</td>
-                    </tr>
-                `;
-            });
-
         })
         .catch(err => console.error("Erro:", err));
 }
 
-
 setInterval(buscarDados, 2000);
-
-
 buscarDados();
+
+function irHistorico() {
+    window.location.href = "historico.html";
+}
+
