03a50294134c2a76f27e9db4079d9604fc56b56b
NykolasDev <166623370+NykolasDev@users.noreply.github.com>
2026-04-23 21:03:33 -0300

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -2,6 +2,12 @@ function login() {
     let usuario = document.getElementById("usuario").value;
     let senha = document.getElementById("senha").value;
 
+    if (usuario === "admin" && senha === "VON") {
+    localStorage.setItem("logado", "true");
+    window.location.href = "Monitoramento.html";
+    return;
+}
+
     let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
     let encontrado = usuarios.find(u => u.usuario === usuario && u.senha === senha);
